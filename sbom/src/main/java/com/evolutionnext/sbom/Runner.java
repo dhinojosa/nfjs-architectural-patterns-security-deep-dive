86cd7e2b1f96746d98af42036e4b6fce5f4f722f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2024-11-14 14:59:12 +0000

    Updated the vulnerabilites and tested app

---
+++ sbom/src/main/java/com/evolutionnext/sbom/Runner.java
@@ -0,0 +1,14 @@
+
+package com.evolutionnext.sbom;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Runner {
+
+    private static final Logger logger = LogManager.getLogger(Runner.class);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        logger.info("Hello World!");
+    }
+}
